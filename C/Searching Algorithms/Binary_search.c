c7be2970900b225da2059a5d90dbea974dda81d6
Sayan3sarkar <sayansarkar333@gmail.com>
2019-07-24 10:17:01 +0530

    Binary Search Added

---
+++ C/Searching Algorithms/Binary_search.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <conio.h>
+#define MAX 20
+
+int Binary_Search(int a[], int n, int item){
+	int beg = 0, end = n-1, mid;
+	mid = (beg+end)/2;
+	while(( beg <= end) && ( a[mid] != item)){
+		if(a[mid]<item)
+			beg = mid+1;
+		else
+			end = mid-1;
+		mid = (beg+end)/2;
+	}
+	if(beg > end)
+		return -1;
+	else
+		return mid;
+}
+
+int main()
+{
+	int i,n, a[MAX],item, pos;
+	while(1)
+	{
+		printf("Enter the size of array:");
+		scanf("%d",&n);
+		if(n>=1 && n<=MAX)
+			break;
+		else
+			printf("\n Size of array must be positive and within 20. Please Enter again by pressing any key.");
+			getch();
+			continue;
+	}
+	//printf("Hello");
+	printf("\n Enter array elements(ascenidng order sorted:"); //Here we assume user enters data keeping the sorting constraint in mind
+	for (i = 0; i < n; i++)
+		scanf("%d", &a[i]);
+	printf("\nArray Elements are:\n");
+	for (i = 0; i < n; i++)
+		printf("%d \t", a[i]);
+	printf("\nEnter number to be searched:");
+	scanf("%d", &item);
+	pos = Binary_Search(a, n, item);
+	if(pos == -1)
+		printf("\n%d is not present in array", item);
+	else
+		printf("\n%d is present at postion %d of array", item, pos+1);
+}
