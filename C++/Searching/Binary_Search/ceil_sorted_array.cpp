a2b759359bb0d5d9bd37348423e3b4e71343d413
Sayan3sarkar <sayansarkar333@gmail.com>
2021-07-11 12:08:31 +0530

    Code added for finding out floor and ceil of a sorted array

---
+++ C++/Searching/Binary_Search/ceil_sorted_array.cpp
@@ -0,0 +1,43 @@
+/******************************************************************************
+
+                              Code By Sayan3sarkar
+
+*******************************************************************************/
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int ceil_sorted_array(vector <int> arr, int target){
+    int n = arr.size();
+    
+    int start = 0, end = n - 1, mid, res = -1;
+    
+    while(start <= end){
+        mid = start + (end - start)/2;
+        
+        if(arr[mid] == target){
+            res = arr[mid];
+            break;
+        }
+        
+        else if(arr[mid] > target){
+            res = arr[mid];
+            end = mid - 1;
+        }
+        
+        else
+            start = mid + 1;
+    }
+    
+    return res;
+}
+
+int main()
+{
+    vector <int> arr{1,2,3,4,8,10,10,12,19};
+    cout<<ceil_sorted_array(arr, 5);
+
+    return 0;
+}
